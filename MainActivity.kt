1ca9866e6b95f9b42318b9bae76b65ebbb8cf3a3
AainaRishu <aainarishu43106@gmail.com>
2019-01-26 22:38:36 +0530

    This app will get an access to the phone's app. if the app is not installed then it will search in playstore to download it.

--- MainActivity.kt
+++ MainActivity.kt
@@ -7,45 +7,52 @@ import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity:AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        b1.setOnClickListener{
+        b1.setOnClickListener {
             var i = Intent()
-            i.action=Intent.ACTION_DIAL
-            i.data= Uri.parse("tel:"+et1.text.toString())
+            i.action = Intent.ACTION_DIAL
+            i.data = Uri.parse("tel:" + et1.text.toString())
             startActivity(i)
 
         }
-        c1.setOnClickListener{
+        c1.setOnClickListener {
             var i = Intent()
-            i.action="android.media.action.IMAGE_CAPTURE"
+            i.action = "android.media.action.IMAGE_CAPTURE"
             startActivity(i)
 
         }
-        c2.setOnClickListener{
+        c2.setOnClickListener {
             var i = Intent()
-            i.action=Intent.ACTION_VIEW
+            i.action = Intent.ACTION_VIEW
             startActivity(i)
 
         }
 
 
 
-        b2.setOnClickListener{
-            var i = Intent(this@MainActivity,
-                WelcomeActivity::class.java)
-            i.putExtra("name",et2.text.toString())
-            i.putExtra("qual",et3.text.toString())
+        b2.setOnClickListener {
+            var i = Intent(
+                this@MainActivity,
+                WelcomeActivity::class.java
+            )
+            i.putExtra("name", et2.text.toString())
+            i.putExtra("qual", et3.text.toString())
             startActivity(i)
 
         }
-        b3.setOnClickListener{
-           var i=Intent()
-            i.action="com.whatsapp"
+        b3.setOnClickListener {
+            val i = packageManager.getLaunchIntentForPackage("com.whatsapp")
+            if (i != null) {   // If App is installed in the phone
+                startActivity(i)
+            } else {
+                val i = Intent()
+                i.action = Intent.ACTION_VIEW
+                i.data = Uri.parse("https://play.google.com/store/apps/details?id=com.whatsapp")
+                startActivity(i)
+            }
         }
     }
 }
